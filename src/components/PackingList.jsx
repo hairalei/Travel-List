ade231d3db102be2ff70efb687db9dbf28c76762
Haira Lei <darhaira@gmail.com>
2023-10-05 21:16:53 +0800

    add sort dropdown

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,11 +1,30 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import Item from './Item';
 
 function PackingList({ list, handlePacked, handleDelete }) {
+  const [sortBy, setSortBy] = useState('date');
+
+  let sortedItems;
+
+  switch (sortBy) {
+    case 'description':
+      sortedItems = list
+        .slice()
+        .sort((a, b) => a.description.localeCompare(b.description));
+      break;
+    case 'packed status':
+      sortedItems = list.slice().sort((a, b) => a.packed - b.packed);
+      break;
+    case 'date':
+      sortedItems = list;
+      break;
+  }
+
   return (
     <div className='list'>
       <ul>
-        {list.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -14,6 +33,14 @@ function PackingList({ list, handlePacked, handleDelete }) {
           />
         ))}
       </ul>
+
+      <div className='actions'>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='date'>Sort by date</option>
+          <option value='description'>Sort by description</option>
+          <option value='packed status'>Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
